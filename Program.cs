d207041e0f136f0b9625fb3f33895283e3fe9101
Aleksander <Greengow@mail.ru>
2021-01-31 19:08:11 +0300

    hvatit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Homework.DaysOfWeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M:
+            Console.WriteLine("Введите день недели");
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case ("Monday"):
+                    Console.WriteLine("Понедельник");
+                    break;
+                case ("Tuesday"):
+                    Console.WriteLine("Вторник");
+                    break;
+                case ("Wednesday"):
+                    Console.WriteLine("Среда");
+                    break;
+                case ("Thursday"):
+                    Console.WriteLine("Четверг");
+                    break;
+                case ("Friday"):
+                    Console.WriteLine("Пятница");
+                    break;
+                case ("Saturday"):
+                    Console.WriteLine("Суббота");
+                    break;
+                case ("Sunday"):
+                    Console.WriteLine("Воскресенье");
+                    break;
+                case ("Exit"):
+                    return;
+                default:
+                    Console.WriteLine("Не день недели");
+                    break;
+            }
+            Console.ReadLine();
+            Console.Clear();
+            goto M;
+        }
+    }
+}
